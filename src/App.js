f1a79004f7573a5526b138e43ea04802e79e5261
masterPhoneGap <tanmoymulti@gmail.com>
2019-04-24 22:53:33 +0530

    in

--- src/App.js
+++ src/App.js
@@ -1,63 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 26 },
-      { name: 'stephanie', age: 29 }
-    ],
-    otherState: 'Some other value',
-    showPersons: false
-  }
-  togglePersonHandler = () => {
-    console.log("toogle");
-    const doesShow =this.state.showPersons;
-    this.setState({showPersons:!doesShow});
-  }
-
-  switchNameHandler = () => {
-    // Do Not Use == this.state.persons[0].name="Arup";
-    this.setState({
-      persons: [
-        { name: 'Maximu', age: 28 },
-        { name: 'Manu', age: 26 },
-        { name: 'stephanie', age: 29 }
-      ]
-    });
-  }
-
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 26 },
-        { name: 'stephanie', age: 29 }
-      ]
-    });
-  }
-
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm  a React App</h1>
-        <p>This is realy works!</p>
-        <button onClick={this.togglePersonHandler}>Switch Name</button>
-        {this.state.showPersons ?
-          <div>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangeHandler} > my habbies: racing </Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-          </div>:null
-        }
-
-
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
